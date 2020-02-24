a383fc9dbd019ed372c80f006d38035570d8192a
Sahaj Arora <sahajarora1286@gmail.com>
2020-02-23 19:29:10 -0500

    Fixed spinner

--- lib/screens/swiper.dart
+++ lib/screens/swiper.dart
@@ -26,6 +26,7 @@ class SwiperController extends StatefulWidget {
 
 class _SwiperControllerState extends State<SwiperController> {
   List<User> users = new List();
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -34,18 +35,25 @@ class _SwiperControllerState extends State<SwiperController> {
   }
 
   _getUsers() async {
+    if (mounted) {
+      setState(() {
+        isLoading = true;
+      });
+    }
     await apiService.getUsers().then((users) {
       // printWrapped("Users: ");
       // print(jsonEncode(users));
       if (mounted) {
         setState(() {
           this.users = users;
+          isLoading = false;
         });
       }
     }).catchError((error) {
       if (mounted) {
         setState(() {
           this.users = new List<User>();
+          isLoading = false;
         });
       }
     });
@@ -66,35 +74,22 @@ class _SwiperControllerState extends State<SwiperController> {
       body: (this.users.length > 0)
           ? new Swiper(
               layout: SwiperLayout.TINDER,
-              // customLayoutOption:
-              //     new CustomLayoutOption(startIndex: -1, stateCount: 3)
-              //         .addRotate([-45.0 / 180, 0.0, 45.0 / 180]).addTranslate([
-              //   new Offset(-370.0, -40.0),
-              //   new Offset(0.0, 0.0),
-              //   new Offset(370.0, -40.0)
-              // ]),
               itemWidth: screenSize.width - 20,
               itemHeight: screenSize.height - 20,
               itemBuilder: (BuildContext context, int index) {
-                // var img = "assets/images/sahaj" + ((index + 1).toString()) + ".jpg";
-                // Profile profile = new Profile(
-                //   img,
-                //   "Sahaj Arora",
-                //   "Development Manager",
-                //   "What's not to like?",
-                // );
                 return new ProfileCardWidget(
                   user: this.users.length > index
                       ? this.users[index]
                       : new User(),
                 );
-                // return new Text("jele");
               },
               itemCount: users.length,
               pagination: null,
               control: null,
             )
-          : new Text('Finding users'),
+          : new Center(
+              child: isLoading ? CircularProgressIndicator() : Center(),
+            ),
     );
   }
 }
